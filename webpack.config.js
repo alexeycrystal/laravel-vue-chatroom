72aabaff817392ea3bf4ebc98a61b94cc0a35fa5
shabramov <alex.shabramov@gmail.com>
2019-02-03 12:05:01 +0200

    Updated - all front-end packages to newer versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
 module.exports = {
+    mode: 'development',
     module: {
-        loaders: [
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
             {
                 test: /\.vue$/,
                 loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
             }
         ]
-    }
-}
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
